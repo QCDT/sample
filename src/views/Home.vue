332169db4b422e14814672d98d4341d310843f72
CacheA <1057440427@qq.com>
2019-06-10 10:50:02 +0800

    jh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 <template>
     <div>
         <div class="homeWrap">
             这是首页
+            kry
+            homeWrap
         </div>
     </div>
 </template>
-=======
->>>>>>> f4a5008891a2e01d0cca32dcd08658d2b81e4aee
-
