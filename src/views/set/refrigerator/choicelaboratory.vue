2d16821fb1396084b8498bc00521c22f0a7efe91
STRIVE <543571284@qq.com>
2019-06-27 09:37:31 +0800

    lyf

---
+++ src/views/set/refrigerator/choicelaboratory.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="labWrap">
+        <div class="labTitle"><span>选择实验室</span></div>
+        <div class="operationlab">
+             <el-button type="primary" size="mini">添加实验室</el-button>
+             <el-button type="primary" size="mini">删除实验室</el-button>
+        </div>
+        <ul class="labList">
+            <li v-for="(item,index) in list" :key="index" :class="{active:activeClass == index}"  @click="chooselab(index)">
+                <img src="@/assets/img/lab.png">
+                <div @dblclick="editLabName(index)" class="labName">
+                    <span v-show="!item.editing">{{item.laboratoryName}}</span>
+                    <el-input v-show="item.editing" v-model="laboratoryName" placeholder="请输入内容" size="mini"></el-input>
+                </div>             
+            </li>
+        </ul>
+        <div class="labBtn">
+             <el-button type="primary" size="mini">保存</el-button>
+             <el-button type="primary" size="mini">返回</el-button>
+        </div>
+        <el-button type="primary" size="mini" class="labNext" @click="equipmentInfo">下一步</el-button>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            laboratoryName:'',
+            activeClass: -1,
+            list:[
+                {
+                    id:Date.now(),
+                    laboratoryName:'实验室',
+                    editing: false
+                },
+                {
+                    id:Date.now(),
+                    laboratoryName:'实验室',
+                    editing: false
+                }
+            ]
+        }
+    },
+    mounted () {
+        // document.onclick = function(){
+        //     this.list.map((item)=>{
+        //        return  item.editing = false;
+        //     })
+        // }
+    },
+    methods: {
+        chooselab (index) {
+            console.log(index)
+            this.activeClass = index;
+            // this.active = true
+        },
+        editLabName (index) {
+            this.list[index].editing = true;
+        },
+        equipmentInfo () {
+            this.$router.push('/set/refrigerator/equipmentInfo')
+        }
+    }
+}
+</script>
+
---
+++ src/views/set/refrigerator/equipmentConstruction.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <div class="equipmentConstruction">
+            <div class="constructionTitle">
+                <span>结构设置</span>
+            </div>
+            <div class="constructionMiddle">
+                <div>
+                    <div><span>层结构:</span></div>
+                    <div>
+                        <span>分层结构:</span>
+                        <el-input v-model="equipmentName" placeholder="请输入内容" size="mini"></el-input>
+                    </div>
+                    <div>
+                        <span>每层结构:</span>
+                        <el-input v-model="equipmentName" placeholder="请输入内容" size="mini"></el-input>
+                        <span>行</span>
+                        <el-input v-model="equipmentName" placeholder="请输入内容" size="mini"></el-input>
+                        <span>列</span>
+                    </div>
+                </div>
+                <div>
+                    <div><span>抽屉结构:</span></div>
+                    <div>
+                        <span>抽屉存放样本盒数量:</span>
+                        <el-input v-model="equipmentName" placeholder="请输入内容" size="mini"></el-input>
+                    </div>
+                    <div>
+                        <span>每个抽屉样本盒数量均为:</span>
+                        <el-input v-model="equipmentName" placeholder="请输入内容" size="mini"></el-input>
+                    </div>
+                </div>
+            </div>
+            <div class="footerBtn">
+                <el-button type="primary"  size="mini">保存</el-button>
+                <el-button type="primary"  size="mini">返回</el-button>
+            </div>
+            <el-button type="primary" class="prevBtn" @click="prevEquipment">上一步</el-button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return{
+            equipmentName: ''
+        }
+    },
+    methods:{
+        prevEquipment () {
+            this.$router.push('/set/refrigerator/equipmentInfo')
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.equipmentConstruction{
+        width: 30%;
+        margin: 0 auto;
+        position: relative;
+    .constructionTitle{
+        text-align: center;
+        margin-top: 20px;
+        font-weight: bold;
+        font-size: 20px;
+    }
+    .constructionMiddle{
+        text-align: center;
+        .el-input{
+            width: 30%;
+            margin-left: 5px;
+            margin-right: 5px;
+        }
+        >div{
+            margin-top: 30px;
+            text-align: left;
+            >div{
+                margin-top: 10px;
+            }
+        }
+    }
+    .footerBtn{
+        text-align: center;
+        margin-top: 30px;
+    }
+    .prevBtn{
+        position: absolute;
+        left: -50%;
+        top:45%;
+    }
+}
+</style>
