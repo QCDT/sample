266866ec714c73484980072e381d07870faebc60
STRIVE <543571284@qq.com>
2020-01-15 13:28:38 +0800

    测试

--- src/views/set/Centrifuge.vue
+++ src/views/set/Centrifuge.vue
@@ -408,7 +408,9 @@ export default {
     // 修改离心机信息
     putProject () {
       console.log(this.eqNumber)
+      console.log(111)
       console.log(11223)
+      console.log('aaa')
       this.$axios({
         method: 'post',
         url:'sampleGuide/centrifuge/updateCentrifuge',
